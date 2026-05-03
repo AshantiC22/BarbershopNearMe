e1938a91488b8cfa9badf03df61002ce32242895
ashanti-cocroft <ashanticocroft23@gmail.com>
2026-05-03 16:59:04 -0500

    fix migration conflict - merge 0002s

---
+++ BACKEND/core/migrations/0003_merge.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0002_alter_appointment_deposit_amount_and_more"),
+        ("core", "0002_newsletter_seen"),
+    ]
+
+    operations = [
+    ]
